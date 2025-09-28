aae72565f6c28edb85e9ccf76a8d1897ef30112c
Kavoyaa <78526936+Kavoyaa@users.noreply.github.com>
2025-09-28 13:31:03 +0530

    Change endpoint name to "all"

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ load_dotenv()
 database_url = os.getenv("DATABASE_URL")
 
 
-@app.get("/")
+@app.get("/all")
 async def root():
     with psycopg.connect(database_url) as conn:
         with conn.cursor() as cur:
