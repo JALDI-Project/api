73e25464c4bf8766813f2a22f0a99be895e016b9
Kavoyaa <78526936+Kavoyaa@users.noreply.github.com>
2025-09-28 19:50:11 +0530

    Implement new endpoints

--- main.py
+++ main.py
@@ -9,10 +9,30 @@ database_url = os.getenv("DATABASE_URL")
 
 
 @app.get("/all")
-async def root():
+async def all():
     with psycopg.connect(database_url) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT * FROM hazards;")
             data = cur.fetchall()
 
+    return {"data": data}
+
+
+@app.get("/hazards/recent")
+async def get_recent_hazards(time: int = 2):
+    with psycopg.connect(database_url) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM hazards WHERE CURRENT_TIMESTAMP - make_interval(hours => %s) < reported_at;", (time, ))
+            data = cur.fetchall()
+            
+    return {"data": data}
+
+
+@app.get("/hazards/{hazard_id}")
+async def get_hazard(hazard_id: int):
+    with psycopg.connect(database_url) as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM hazards WHERE hazard_id=%s;", (hazard_id, ))
+            data = cur.fetchone()
+    
     return {"data": data}
