8083b18af8c2e9c68656a6a24068a720b98ffddb
Kavoya <78526936+Kavoyaa@users.noreply.github.com>
2025-10-26 19:02:07 +0530

    Add CORS support

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 import os
 import psycopg
 
+
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 load_dotenv()
 database_url = os.getenv("DATABASE_URL")
 
